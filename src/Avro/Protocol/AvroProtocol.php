78f884efbd2de49018c8ebdfee4088aeeb8c25dc
Farrokh Ghamsary <fghamsary@v3d.fr>
2019-12-19 15:37:42 +0100

    Error log for the serialization of unknown array type value

--- src/Avro/Protocol/AvroProtocol.php
+++ src/Avro/Protocol/AvroProtocol.php
@@ -216,6 +216,7 @@ class AvroProtocol {
                   $firstValue = $innerValue->current();
                 } else {
                   $firstValue = null;
+                  error_log("There is a problem with value for JSON serialization in $name for the object of type $fieldType in schema $fullName: " . json_encode($innerValue));
                 }
                 if ($firstValue instanceof IAvroRecordBase) {
                   $serializedObject[$name] = [];
